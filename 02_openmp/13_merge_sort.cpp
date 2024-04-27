74fed01b80e8602b6d8bc34febaaa4b2d69d0187
tatyam <tatuhito0125@yahoo.co.jp>
2024-04-28 04:16:30 +0900

    なぜか vec が shared にならない (なんで？)

--- 02_openmp/13_merge_sort.cpp
+++ 02_openmp/13_merge_sort.cpp
@@ -24,9 +24,9 @@ void merge(std::vector<int>& vec, int begin, int mid, int end) {
 void merge_sort(std::vector<int>& vec, int begin, int end) {
     if (begin < end) {
         int mid = (begin + end) / 2;
-        #pragma omp task
+        #pragma omp task shared(vec)
         merge_sort(vec, begin, mid);
-        #pragma omp task
+        #pragma omp task shared(vec)
         merge_sort(vec, mid + 1, end);
         #pragma omp taskwait
         merge(vec, begin, mid, end);
@@ -34,7 +34,7 @@ void merge_sort(std::vector<int>& vec, int begin, int end) {
 }
 
 int main() {
-    int n = 10;
+    int n = 1e6;
     std::vector<int> vec(n);
     for (int i = 0; i < n; i++) {
         vec[i] = rand() % (10 * n);
