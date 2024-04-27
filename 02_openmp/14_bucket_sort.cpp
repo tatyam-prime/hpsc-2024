fbb3f38c16d8af9bbee232d723095e8fcccf227d
tatyam <tatuhito0125@yahoo.co.jp>
2024-04-28 05:57:32 +0900

    parallel prefix sum を実装

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,33 +1,51 @@
+#include <cassert>
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
 
+const int n = 1e7;
+const int range = 1e5;
+int key[n];
+int bucket[range + 1], offset[range + 1];
 int main() {
-    int n = 1e7;
-    int range = 1e6;
-    std::vector<int> key(n);
     for (int i = 0; i < n; i++) {
         key[i] = rand() % range;
         printf("%d ", key[i]);
     }
     printf("\n");
+    
+    // std::copy(std::begin(key), std::end(key), std::begin(ans));
+    // std::sort(std::begin(ans), std::end(ans));
 
-    std::vector<int> bucket(range, 0);
-    for (int i = 0; i < n; i++)
-        bucket[key[i]]++;
-    std::vector<int> offset(range, 0);
-    for (int i = 1; i < range; i++)
-        offset[i] = offset[i - 1] + bucket[i - 1];
-#pragma omp parallel for
+    #pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        #pragma omp atomic
+        bucket[key[i] + 1]++;
+    }
+
+    // parallel prefix sum
+    #pragma omp parallel for
+    for (int i = 0; i <= range; i++) offset[i] = bucket[i];
+    #pragma omp parallel
+    for (int w = 1; w < range; w <<= 1) {
+        #pragma omp for
+        for (int i = 1; i <= range; i++) bucket[i] = offset[i];
+        #pragma omp for
+        for (int i = w; i <= range; i++) offset[i] += bucket[i - w];
+    }
+
+    assert(offset[range] == n);
+
+    #pragma omp parallel for
     for (int i = 0; i < range; i++) {
-        int j = offset[i];
-        for (; bucket[i] > 0; bucket[i]--) {
-            key[j++] = i;
-        }
+        #pragma omp parallel for
+        for (int j = offset[i]; j < offset[i + 1]; j++) key[j] = i;
     }
 
     for (int i = 0; i < n; i++) {
         printf("%d ", key[i]);
     }
     printf("\n");
+
+    // for(int i = 0; i < n; i++) assert(key[i] == ans[i]);
 }
